02dbc46412f658ba35eae76e8133e64efb8413ef
KaileyJo <kaileycolestock@Kaileys-MacBook-Pro.local>
2016-02-07 21:32:40 -0600

    Timed switch added; timer needs to reset and <li>s need to link with profiles still

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
     getData();
 
-    var spin = window.setInterval(carouselSpin, 5000);
+    var spin = window.setInterval(carouselSpin, 10000);
+
     $('#next').on('click', function(){
-        var lastPerson = $('.person').last();
-        if (lastPerson.hasClass('show')) {
-            $('.person').first().addClass('show');
-            $('.person').last().removeClass('show');
-        } else {
-            $('.show').next().addClass('show');
-            $('.show').first().removeClass('show');
-        }
+        nextSlide();
     });
+
     $('#previous').on('click', function(){
         var firstPerson = $('.person').first();
         if (firstPerson.hasClass('show')) {
@@ -22,6 +17,14 @@ $(document).ready(function(){
             $('.show').last().removeClass('show');
         }
 
+    });
+
+    //$('.show').
+
+    $('li').on('click', function(){
+        var dataIndex = this.dataset.index;
+        console.log(dataIndex);
+        //$(this).addClass('active');
     });
 
 });
@@ -40,7 +43,7 @@ function getData(){
                 $el.append('<p>' + person.favoriteMovie1 + '</p>');
                 $el.append('<p>' + person.favoriteMovie2 + '</p>');
                 $el.append('<p>' + person.favoriteSong + '</p>');
-                $('ul').append('<li data-slide-to="' + i + '"></li>');
+                $('ul').append('<li data-index="' + i + '"></li>');
             });
             $('.person').first().addClass('show');
         },
@@ -52,6 +55,24 @@ function getData(){
 }
 
 function carouselSpin() {
+    nextSlide();
+};
 
+function nextSlide() {
+    var lastPerson = $('.person').last();
+    if (lastPerson.hasClass('show')) {
+        $('.person').first().addClass('show');
+        $('.person').last().removeClass('show');
+    } else {
+        $('.show').next().addClass('show');
+        $('.show').first().removeClass('show');
+    }
+    //var $visiblePerson = $('.show').data('index');
+    //var $activeLi = $('li').data('index');
+    //if ($visiblePerson == $activeLi) {
+    //    $activeLi.addClass('active');
+    //} else {
+    //    $('li').removeClass('active');
+    //}
 
 };
